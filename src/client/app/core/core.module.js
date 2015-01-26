6083bc66835a93f369d85d8b519fbbd07b187fa3
Eduardo Zamora Blandino <ezamora@Eduardos-MacBook-Pro.local>
2015-01-25 21:08:38 -0600

    updates to dashboard

--- src/client/app/core/core.module.js
+++ src/client/app/core/core.module.js
@@ -5,7 +5,7 @@
         .module('app.core', [
             'ngAnimate', 'ngSanitize',
             'blocks.exception', 'blocks.logger', 'blocks.router',
-            'ui.router', 'ngplus', 'ui.bootstrap', 'mgo-angular-wizard', 'ngStorage', 'internationalPhoneNumber', 'ngTagsInput'
+            'ui.router', 'ui.utils', 'ngplus', 'ui.bootstrap', 'mgo-angular-wizard', 'ngStorage', 'internationalPhoneNumber', 'ngTagsInput'
         ])
         .run(function($rootScope, $state, authToken){
             $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
